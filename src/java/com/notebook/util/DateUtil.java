92a6504924e3dd7b674a7ebb17ed2b4b24cb6d91
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-03-26 01:12:01 -0300

    add toCalendar method

--- src/java/com/notebook/util/DateUtil.java
+++ src/java/com/notebook/util/DateUtil.java
@@ -7,4 +7,10 @@ public class DateUtil {
     public static Date toSQLDate(Calendar date) {
         return new Date(date.getTimeInMillis());
     }
+    
+    public static Calendar toCalendar(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar;
+    }
 }
